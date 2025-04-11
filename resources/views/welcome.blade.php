7833fbf8cec39ee03d846b9ab8c304ab97f51b86
Arifa-Infiroz <507arifa96@gmail.com>
2025-04-11 09:04:32 +0530

    add ani

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-        <h1>hello</h1>
+        <h1>hello  jkhkuhk</h1>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
