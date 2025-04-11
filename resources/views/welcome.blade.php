c7b2fa4734de03bf5dc260f9aca6ad4f2c55806b
Arifa-Infiroz <507arifa96@gmail.com>
2025-04-11 08:02:00 +0530

    chenge in welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        <h1>hello</h1>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
